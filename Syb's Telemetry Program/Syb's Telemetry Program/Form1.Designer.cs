776e6bbf41665e6aed5c31ab324fea9acace99af
Sybren Postma <sybdegamer@gmail.com>
2021-11-20 20:17:03 +0100

    COM port reading upload but bugged

--- Syb's Telemetry Program/Syb's Telemetry Program/Form1.Designer.cs
+++ Syb's Telemetry Program/Syb's Telemetry Program/Form1.Designer.cs
@@ -29,25 +29,26 @@ namespace Syb_s_Telemetry_Program
         /// </summary>
         private void InitializeComponent()
         {
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.ConsoleWindow = new System.Windows.Forms.RichTextBox();
             this.StartSessionButton = new System.Windows.Forms.Button();
             this.EndSessionButton = new System.Windows.Forms.Button();
             this.ComListBox = new System.Windows.Forms.ListBox();
             this.SelectFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.AutoScrollButton = new System.Windows.Forms.CheckBox();
+            this.TimeStampButton = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
-            // richTextBox1
+            // ConsoleWindow
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(488, 12);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(300, 426);
-            this.richTextBox1.TabIndex = 0;
-            this.richTextBox1.Text = "";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            this.ConsoleWindow.Location = new System.Drawing.Point(648, 12);
+            this.ConsoleWindow.Name = "ConsoleWindow";
+            this.ConsoleWindow.Size = new System.Drawing.Size(300, 501);
+            this.ConsoleWindow.TabIndex = 4;
+            this.ConsoleWindow.Text = "";
             // 
             // StartSessionButton
             // 
-            this.StartSessionButton.Location = new System.Drawing.Point(488, 418);
+            this.StartSessionButton.Location = new System.Drawing.Point(648, 493);
             this.StartSessionButton.Name = "StartSessionButton";
             this.StartSessionButton.Size = new System.Drawing.Size(150, 20);
             this.StartSessionButton.TabIndex = 1;
@@ -57,7 +58,7 @@ namespace Syb_s_Telemetry_Program
             // 
             // EndSessionButton
             // 
-            this.EndSessionButton.Location = new System.Drawing.Point(638, 418);
+            this.EndSessionButton.Location = new System.Drawing.Point(798, 493);
             this.EndSessionButton.Name = "EndSessionButton";
             this.EndSessionButton.Size = new System.Drawing.Size(150, 20);
             this.EndSessionButton.TabIndex = 2;
@@ -67,37 +68,61 @@ namespace Syb_s_Telemetry_Program
             // ComListBox
             // 
             this.ComListBox.FormattingEnabled = true;
-            this.ComListBox.Items.AddRange(new object[] {
-            "COM1",
-            "COM2",
-            "COM3"});
-            this.ComListBox.Location = new System.Drawing.Point(362, 343);
+            this.ComListBox.Location = new System.Drawing.Point(522, 418);
             this.ComListBox.Name = "ComListBox";
             this.ComListBox.Size = new System.Drawing.Size(120, 95);
             this.ComListBox.TabIndex = 3;
+            // 
+            // AutoScrollButton
+            // 
+            this.AutoScrollButton.AutoSize = true;
+            this.AutoScrollButton.Location = new System.Drawing.Point(562, 395);
+            this.AutoScrollButton.Name = "AutoScrollButton";
+            this.AutoScrollButton.Size = new System.Drawing.Size(77, 17);
+            this.AutoScrollButton.TabIndex = 5;
+            this.AutoScrollButton.Text = "Auto Scroll";
+            this.AutoScrollButton.UseVisualStyleBackColor = true;
+            // 
+            // TimeStampButton
+            // 
+            this.TimeStampButton.AutoSize = true;
+            this.TimeStampButton.Location = new System.Drawing.Point(562, 372);
+            this.TimeStampButton.Name = "TimeStampButton";
+            this.TimeStampButton.Size = new System.Drawing.Size(77, 17);
+            this.TimeStampButton.TabIndex = 6;
+            this.TimeStampButton.Text = "Timestamp";
+            this.TimeStampButton.UseVisualStyleBackColor = true;
             // 
             // SybsTelemetry
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(960, 525);
+            this.Controls.Add(this.TimeStampButton);
+            this.Controls.Add(this.AutoScrollButton);
             this.Controls.Add(this.ComListBox);
             this.Controls.Add(this.EndSessionButton);
             this.Controls.Add(this.StartSessionButton);
-            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.ConsoleWindow);
             this.Name = "SybsTelemetry";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Syb\'s Telemetry Program";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox ConsoleWindow;
         private System.Windows.Forms.Button StartSessionButton;
         private System.Windows.Forms.Button EndSessionButton;
         private System.Windows.Forms.ListBox ComListBox;
         private System.Windows.Forms.FolderBrowserDialog SelectFolder;
+        private System.Windows.Forms.CheckBox AutoScrollButton;
+        private System.Windows.Forms.CheckBox TimeStampButton;
     }
 }
 
--- Syb's Telemetry Program/Syb's Telemetry Program/Form1.cs
+++ Syb's Telemetry Program/Syb's Telemetry Program/Form1.cs
@@ -7,14 +7,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
 using System.IO.Ports;
 
 namespace Syb_s_Telemetry_Program
 {
+    public static class ENV
+    {
+        public static int bautrate = 11500;
+    }
     
     public partial class SybsTelemetry : Form
     {
+        public bool sessionStarted = false;
+        public static SerialPort port = new SerialPort("COM1", ENV.bautrate, Parity.None, 8, StopBits.One);
+
         public async Task ComPortLoop()
         {
             string[] lastPorts = { };
@@ -23,58 +29,95 @@ namespace Syb_s_Telemetry_Program
                 //Serial port listing
                 string[] ports = SerialPort.GetPortNames();
 
-                if (ports != lastPorts)
+                foreach (string port in ports)
                 {
-                    foreach (string port in ports)
+                    //ConsoleLog($"Port found: {port}");
+                    if (!lastPorts.Contains(port))
                     {
-                        if (!ComListBox.Items.Contains(port))
-                        {
-                            ComListBox.Items.Add(port);
-                        }
-                    }
-
-                    foreach (string listPort in ComListBox.Items)
-                    {
-                        if (ports.Contains(listPort))
-                        {
-                            ComListBox.Items.Remove(listPort);
-                        }
+                        //ading port to list because it was not found in the previous loop
+                        //ConsoleLog($"Adding: {port}");
+                        ComListBox.Items.Add(port);
                     }
                 }
 
+                foreach (string port in lastPorts)
+                {
+                    if (!ports.Contains(port))
+                    {
+                        // removing because port was not found in the new list
+                        //ConsoleLog($"Removing: {port}");
+                        ComListBox.Items.Remove(port);
+                    }
+                }
+
+                lastPorts = ports;
                 //500ms delay for loop speed
-                await Task.Delay(500);
+                await Task.Delay(100);
             }
         }
 
         public async Task ComReceiver()
         {
+            string lastPort = string.Empty;
+            while (true)
+            {
+                if (ComListBox.SelectedItem != null)
+                {
+                    string selectedPort = ComListBox.SelectedItem.ToString();
+                    if (selectedPort != lastPort && sessionStarted)
+                    {
+                        if (port.IsOpen)
+                        {
+                            port.Close();
+                        }
+                        port.PortName = selectedPort;
+                        port.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+                        port.Open();
+                    }
+                }
+            }
+
             await Task.Delay(0);
+        }
+
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            ConsoleLog($"COMPort Data: {port.ReadExisting()}");
         }
 
         public void ConsoleLog(string message)
         {
-
+            var console = ConsoleWindow;
+            if (console.Text == "")
+            {
+                console.Text = console.Text + message;
+            }
+            else
+            {
+                console.Text = console.Text + "\n" + message;
+            }
         }
 
+        public void StartSession()
+        {
+
+        }
+        
+        public void StopSession()
+        {
+
+        }
         public SybsTelemetry()
         {
             InitializeComponent();
-            _ = ComPortLoop();
-            _ = ComReceiver();
-        }
-
-        private void richTextBox1_TextChanged(object sender, EventArgs e)
-        {
-
+            ComPortLoop();
+            ComReceiver();
         }
 
         private void StartSessionButton_Click(object sender, EventArgs e)
         {
             SelectFolder.ShowDialog();
+            sessionStarted = true;
         }
-
-        
-
     }
 }
